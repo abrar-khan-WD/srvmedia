d85bbb07078331931fba13d7c25a4f6809031646
Abrar Khan <150218710+abrar-khan-WD@users.noreply.github.com>
2024-12-05 13:19:03 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -5,19 +5,8 @@
             <div class="row">
                 <div class="col-md-12 text-center">
                     <div class="d-flex justify-content-center align-items-center py-3">
-                        <!-- <img src="images/design/logo/4.webp" class="hero_logo_01" loading="lazy" alt="">
-                        <div class="footerImgDivider"></div>
-                        <img src="images/design/logo/naac-footer.webp" class="footer_logo_02" loading="lazy" alt=""> -->
-                        Logo
-                    </div>
-                    <!-- <div class="py-2">
-                        <p class="textBrown1">Address:</p>
-                        <p>DY Patil University Sector 7. Nerul, Mumbai -400706. INDIA</p>
-                    </div> -->
-                    <div class="footerDivider"></div>                    
-                    <span class=" text-light">© Copyright 2025, Designed and developed by
-                        <img src="images/design/logo/gladowl_logo.webp" height="20" width="auto"  loading="lazy" alt=""></span>
-                </div>
+                                      </div>
+           
             </div>
         </div>
     </div>
